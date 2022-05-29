ce78b53583ac669abd9d219ec9b22413d17ab81a
Graham Hughes <graehu@gmail.com>
2022-05-29 09:23:33 +0100

    Basic Matches
    * adds handling for basic options
    
    Need to add to an iterative matching, with different regexs.

--- clpy.py
+++ clpy.py
@@ -6,15 +6,23 @@ import re
 program_name="clpy"
 description="""Convert a CLI to a python module."""
 version="0.0.1"
-whitespace = re.compile("^\s")
-option = re.compile("--([A-Za-z0-9\-]+)(?:\s+)?(.*?\n)")
-short_option = re.compile("^\s+-([A-Za-z0-9]+)(?:(=| )([A-Za-z0-9]+))?")
-long_option = re.compile("(?:^\s+|, )--([A-Za-z0-9\-]+)(?:(=| )([A-Za-z0-9\-]+))?")
+class reg:
+    whitespace = re.compile("^\s")
+    word = re.compile("[A-Za-z0-9]+")
+    optional = re.compile("\[=?([A-Za-z0-9]+)\]")
+    # arg = re.compile("^\s+-(?:-)?([A-Za-z0-9\-]+)(?:\[?(=| )\[?([A-Za-z0-9\-<>]+)\]?(?:, |\s\s))?")
+    arg = re.compile("^\s+--?([A-Za-z0-9\-]+)" + # --switch
+                     "(?:\[?(=| )\[?([A-Za-z0-9\-<>]+)\]?" + #[=<optional>]
+                     ", |\s\s)?") # ensure ending whitespace
+    long_arg = re.compile("--([A-Za-z0-9\-]+)(?:(=| )([A-Za-z0-9\-<>]+))?")
 
 class OptionMeta:
     def str(option):
         out = []
+        
         # todo: do rjusts on on titles instead of manual spaces
+        if option.lines:
+            out.append("'"+"'\n'".join(option.lines)+"'\n")
         if option.opts:
             out.append("options: "+option.opts)
         if option.short_opt:
@@ -25,55 +33,68 @@ class OptionMeta:
         if option.doc:
             out.append("docs:    "+("\n"+tab).join(option.doc))
         if out:
-            out.append("---")
+            out.append("}"+"".ljust(64, '-')+"{")
         return "\n".join(out)
         
 
 class Option:
+    lines = None
     opts = None
     short_opt = None
     long_opt = None
     doc = None
     def __init__(self):
         self.doc = []
+        self.lines = []
 
 def main():
     parser = argparse.ArgumentParser(prog=program_name, description=description)
     parser.add_argument("-v", "--version", action="version", version=version)
     parser.add_argument("-c", "--command", required=True, help="the command to convert to a module")
+    parser.add_argument("-t1", "--test1", help="complex arg for testing")
+    parser.add_argument("-t2", "--test2", nargs=3, help="complex arg for testing")
+    parser.add_argument("-t3", "--test3", nargs="+",help="complex arg for testing")
+    parser.add_argument("-t4", "--test4", nargs="*",help="complex arg for testing")
     args = parser.parse_args()
     help_text = subprocess.getoutput(args.command+" --help").split("\n")
     cur_option = None
+    header = []
+    unused = []
+    options = []
     for line in help_text:
-        match = short_option.search(line)
-        if match:
-            if cur_option:
-                print(OptionMeta.str(cur_option))
+        match = reg.arg.search(line)
+        if match and reg.word.search(match.group(1)):
             cur_option = Option()
+            options.append(cur_option)
+            cur_option.lines.append(line)
             cur_option.short_opt = match
-            split_str = [x for x in match.groups() if x][-1]
             pos = match.span()[1]
-            # print(line)
-            # print("short: "+str(match.groups()))
-            match = long_option.search(line)
+            match = reg.long_arg.search(line, pos)
             cur_option.long_opt = match
             if match:
-                split_str = [x for x in match.groups() if x][-1]
                 pos = match.span()[1]
-                # print("long: "+str(match.groups()))
-            # print("split: "+str(split_str))
-            print(pos)
             opts, doc = line[:pos], line[pos:]
-
-            # print("doc: "+doc.strip())
             if doc.strip():
                 cur_option.doc.append(doc.strip())
             cur_option.opts = opts.strip()
-        elif cur_option and whitespace.search(line):
+        elif cur_option and reg.whitespace.search(line):
+            cur_option.lines.append(line)
             stripped = line.strip()
             if stripped:
                 cur_option.doc.append(stripped)
-    print(OptionMeta.str(cur_option))
+        elif not options:
+            header.append(line)
+        else:
+            # Note: This is potentially dangerous.
+            cur_option = None
+            unused.append(line)
+
+    print("\n".join(header))
+    print("----------------")
+    for o in options:
+        print(OptionMeta.str(o))
+    print("----------------")
+    print("\n".join(unused))
 
 if __name__ == "__main__":
     main()
